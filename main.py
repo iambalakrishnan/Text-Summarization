92a6c85f1c60e1d3291c04a4d544b8171d5a3c20
iambalakrishnan <imbkrishnaa@gmail.com>
2023-06-04 19:37:52 +0530

    main file and data file update

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+from src.textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
 from src.textSummarizer.logging.log import logger
 
-logger.info("Custom logging")
+STAGE_NAME ="Stage 01 - Data Ingestion "
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} finished <<<<<< \n\n {120*'='}",)
+except Exception as e:
+    logger.exception(e)
+    raise e
