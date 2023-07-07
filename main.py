41331577f67a7b03b38601cfb6203bc2e983fdca
iambalakrishnan <imbkrishnaa@gmail.com>
2023-07-07 18:24:57 +0530

    model trainer main update

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from src.textSummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from src.textSummarizer.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 from src.textSummarizer.logging.log import logger
 
 
@@ -37,6 +38,18 @@ try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     data_transformation = DataTransformationTrainingPipeline()
     data_transformation.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} finished <<<<<< \n\n {120*'='}",)
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME ="Stage 04 - Model Trainer "
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    model_trainer = ModelTrainerTrainingPipeline()
+    model_trainer.main()
     logger.info(f">>>>>> stage {STAGE_NAME} finished <<<<<< \n\n {120*'='}",)
 except Exception as e:
     logger.exception(e)
